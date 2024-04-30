cf19c244ec4607e0a2d8f822bed91fa88921c710
twscode <67377559+twscode@users.noreply.github.com>
2024-04-30 13:15:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.querySelector('.burger').addEventListener('click', function() {
     document.querySelector('.nav-links').classList.toggle('nav-active');
-    // Burger Animation
     document.querySelector('.burger').classList.toggle('toggle');
 });
