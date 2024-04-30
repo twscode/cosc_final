d42fa9794963cc0ccd20afa795a02ddf25f611a0
twscode <67377559+twscode@users.noreply.github.com>
2024-04-30 13:15:13 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector('.burger').addEventListener('click', function() {
+    document.querySelector('.nav-links').classList.toggle('nav-active');
+    // Burger Animation
+    document.querySelector('.burger').classList.toggle('toggle');
+});
